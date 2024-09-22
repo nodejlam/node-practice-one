a6b4482407718cf69ba6d014e126d8d8c5b14f9a
Lavzmz <mobiaffiliateone@gmail.com>
2024-09-22 18:39:34 -0500

    Creating API

---
+++ src/controllers/car.controllers.js
@@ -0,0 +1,52 @@
+
+
+const catchError = require('../utils/catchError');
+const Car = require('../models/Car');
+
+const getAll = catchError(async(req, res) => {
+    // Operaciones...
+    const result = await Car.findAll();// select * from cars;
+    return res.status(200).json(result);
+});
+
+const create = catchError(async (req, res) => {
+    const result = await Car.create(req.body);
+    return res.status(201).json(result);
+});
+
+const getOne = catchError(async (req, res) => {
+    const {id} = req.params;
+    const result = await Car.findByPk(id);
+    if (!result) return res.sendStatus(404);
+    return res.status(200).json(result);
+});
+
+const destroy = catchError( async (req, res) =>{
+    const {id} = req.params;
+    const result = await Car.destroy({where: {id}});
+    if(!result) return res.staus(404).json({"message": "Car not found"});
+    return res.sendStatus(204);
+});
+
+const update =catchError(async (req, res) => {
+    const {id} = req.params;
+    const result = await Car.update(
+        req.body, 
+        {where: {id}, 
+        returning:true});
+    if(result[0] === 0) return res.status(404).json({"message": "Car not found"});
+    return res.status(200).json(result[1][0]);
+
+
+    
+});
+
+
+
+module.exports = {
+    getAll,
+    create,
+    getOne,
+    destroy,
+    update,
+};
---
+++ src/models/Car.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../utils/connection'); 
+   // En Mayúsculas y singular      // en minúsculas y singular
+const Car = sequelize.define('car', {
+    // Definimos las columnas aquí
+    brand: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    model: {
+        type: DataTypes.STRING(100)
+        // allowNull por defecto esta en true
+    },
+    year: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    color: {
+        type: DataTypes.STRING,
+        allowNull: false
+}
+});
+
+module.exports = Car;
